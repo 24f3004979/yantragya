bc70d271fd752da92e85ac9befa2947c86c5027a
24f3004979 <madhavprapanna@gmail.com>
2026-08-01 22:24:28 +0530

    [gradient-update-doc-string] gradient descent update

--- src/LinearRegression/GradientDescent.py
+++ src/LinearRegression/GradientDescent.py
@@ -2,8 +2,10 @@
 Gradient Descent algorithm
 A sequential way for building weights for given dataset
 
-Introducing new domains to handle
-    epotch handle
-    tuning hyper-parameters
+Approach
+1. Prediction with seed weight
+2. Gradient computation with respect to prediction
+3. update logic with each iteration
 '''
+
 

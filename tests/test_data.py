784d0b1e743be21d465f515b874133dc86f17dff
24f3004979 <madhavprapanna@gmail.com>
2026-08-13 13:43:09 +0530

    refactor node

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,6 +7,8 @@ initiate simple noisy dataset for the linear regression testing
 #from src.LinearRegression.OrdinaryLeastSquare import OrdinaryLeastSquare
 from src.LinearRegression.GradientDescent import GradientDescent
 
+from src.LinearRegression.OrdinaryLeastSquare import OrdinaryLeastSquare
+
 from src.utility.CsvLoader import CSVHandle
 import numpy as np 
 # vizion module is currently broken and moved for archival :) | Pririty shitft 
@@ -14,10 +16,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-
-
-
-X,Y = CSVHandle('/home/madhav/workspace/projects/yantragya/linear_regress.csv', "y").load()
+X,Y = CSVHandle('/home/madhav/workspace/projects/yantragya/tests/test_data.csv', "y").load()
 
 training_data = {
         "X" : X,
@@ -25,6 +24,28 @@ training_data = {
         } # json :)
 
 seed_weight = [3,3]
-gradient_model = GradientDescent(training_data, seed_weight)
-p = gradient_model.y_cap(seed_weight)
-gradient_model.train(10, 0.2)
+plt.scatter(X, Y)
+
+model = OrdinaryLeastSquare(X, Y)
+weights = model.train_weight()
+
+print(f"Predicted Weights  : {weights}")
+
+X_flatten = X.flatten()
+first = X_flatten[1]
+last = X_flatten[-1]
+print(f'first : {first, last}')
+pred1 = (first * weights[1]) + weights[0]
+pred2 = (last * weights[1]) + weights[0]
+predictions = [[first, last], [pred1, pred2]]
+
+print(f"Predictions : {predictions}")
+cords = [first, last]
+preds = [pred1, pred2]
+plt.plot(cords, preds, color='red')
+plt.show()
+
+#gradient_model = GradientDescent(training_data, seed_weight)
+#p = gradient_model.y_cap(seed_weight)
+
+#gradient_model.train(10, 0.2)
